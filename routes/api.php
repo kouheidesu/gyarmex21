5dda6c16e7fd6926e15b13f58b71366a9898961b
kouheidesu <gyarmex@gmail.com>
2021-07-20 14:15:39 +0900

    20210720 kazihara

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::apiResource('/v1/rest', RestController::class);
+
 });
+
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'Hello'
+    ], 200);
+});
+
