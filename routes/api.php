1e279c62fd44df137ba3fc66aabc6968e3db8d46
kouheidesu <gyarmex@gmail.com>
2021-07-20 16:26:23 +0900

    20210720

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();  
 });
+
 Route::apiResource('/v1/rest', RestController::class);
 
 Route::get('/hello', function () {
